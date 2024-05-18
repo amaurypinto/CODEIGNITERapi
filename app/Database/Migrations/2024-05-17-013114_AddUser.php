222518d8fb1c149b069f168bd0694c22dc9a67e2
amaurypinto <131736596+amaurypinto@users.noreply.github.com>
2024-05-17 19:42:05 -0500

    nuevo cambio bd
    se hizo la estructura de la base de datos

--- app/Database/Migrations/2024-05-17-013114_AddUser.php
+++ app/Database/Migrations/2024-05-17-013114_AddUser.php
@@ -8,11 +8,38 @@ class AddUser extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id'=>[
+                'type'=>'BIGINT',
+                'constraint'=>225,
+                'unsigned'=>true,
+                'auto_increment'=>true
+            ],
+            'email'=>[
+                'type'=>'VARCHAR',
+                'unique'=>true,
+                'constraint'=>225,
+            ],
+            'password'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>225,
+            ],
+            'create_at'=>[
+                'type'=>'TIMESTAMP',
+                'null'=>true
+            ],
+            'updated_at'=>[
+                'type'=>'TIMESTAMP',
+                'null'=>true
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('users');
+
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('users');
     }
 }
